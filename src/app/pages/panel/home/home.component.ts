64be8cefbaf06c61ff8f5f73d7f0467b8da2e6d5
amir <mail.hampacredit.com>
2023-02-19 12:28:08 +0330

    rollback changes

--- src/app/pages/panel/home/home.component.ts
+++ src/app/pages/panel/home/home.component.ts
@@ -16,10 +16,4 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  handleClick() {
-    window.open(
-      'https://app.vee.ir/callback?type=contract&&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIwMDEyMDkxODU1IiwiYXV0aG9yaXR5IjoidXNlcl9wYW5lbCIsImlzcyI6IkhhbXJhaCBMb3R1cyIsImV4cCI6MTY3Njc5Njk4OSwidXNlcklkIjo5NTU2LCJpc1Bhc3N3b3JkU2V0Ijp0cnVlLCJpYXQiOjE2NzY3OTYzODl9.Lnvdjm2E7fXob6CW8rrVhbBOGLBxvUWGsblLn_DrUkiV8ONwxe7r_K6it5KKn-pUjvynI2E3u3ITj12AovjbKw',
-      '_parent'
-    )
-  }
 }
