2c879c6e943ad7f5abc71ee43b26961e4490431d
lenovo <amirheydari9@gmail.com>
2023-02-28 23:29:58 -0800

    handle date picker

--- src/app/ui-kit/date-picker/date-picker.component.ts
+++ src/app/ui-kit/date-picker/date-picker.component.ts
@@ -1,53 +1,27 @@
 import {Component, Input, NgModule, OnInit, Self} from '@angular/core';
 import {BaseControlValueAccessor} from "../../utils/BaseControlValueAccessor";
 import {FormControl, FormsModule, NgControl} from "@angular/forms";
-import {DateService} from "../../service/date.service";
 import {MatDatepickerInputEvent, MatDatepickerModule} from "@angular/material/datepicker";
 import {FieldErrorModule} from "../field-errors/field-errors.component";
 import {CommonModule} from "@angular/common";
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-  MatNativeDateModule,
-  NativeDateAdapter
-} from "@angular/material/core";
+import {MAT_DATE_FORMATS, MatNativeDateModule} from "@angular/material/core";
+import {MatMomentDateModule} from "@angular/material-moment-adapter";
+import * as _moment from 'moment';
+
+const moment = _moment;
 
 
-const DATE_FORMATS = {
+const MY_FORMATS = {
   parse: {
-    dateInput: 'YYYY-MM-DD',
+    dateInput: 'DD MMMM YYYY',
   },
   display: {
     dateInput: 'YYYY-MM-DD',
-    monthYearLabel: 'MMMM YYYY',
+    monthYearLabel: 'YYYY MMM',
     dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY'
-  }
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
 };
-
-class AppDateAdapter extends NativeDateAdapter {
-  public constructor(matDateLocale: string) {
-    super(matDateLocale);
-  }
-
-  public override format(date: Date, displayFormat: string): string {
-    // if (displayFormat === 'YYYY-MM-DD') {
-    //   return date.toLocaleDateString('en-GB', {
-    //     year: 'numeric',
-    //     month: '2-digit',
-    //     day: '2-digit',
-    //   });
-    // } else {
-    //   return date.getFullYear().toString();
-    // }
-    return date.toLocaleDateString('en-GB', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    });
-  }
-}
 
 
 @Component({
@@ -66,7 +40,6 @@ export class DatePickerComponent extends BaseControlValueAccessor<string> implem
 
   constructor(
     @Self() private controlDirective: NgControl,
-    private dateService: DateService
   ) {
     super()
     controlDirective.valueAccessor = this;
@@ -77,14 +50,8 @@ export class DatePickerComponent extends BaseControlValueAccessor<string> implem
   }
 
   onChanged($event: MatDatepickerInputEvent<any | null>) {
-    const value =  $event.value.toLocaleDateString('en-GB', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    });
-    // const value = this.dateService.convertGeorgianToJalali(
-    //   $event.value.toISOString(), 'YYYY-MM-DD', 'iso')
-    this.changed(value);
+    const date = moment($event.value).format('YYYY-MM-DD')
+    this.changed(date);
   }
 }
 
@@ -96,13 +63,12 @@ export class DatePickerComponent extends BaseControlValueAccessor<string> implem
     MatDatepickerModule,
     FormsModule,
     FieldErrorModule,
-    // MaterialJalaliMomentAdapterModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatMomentDateModule,
   ],
   exports: [DatePickerComponent],
   providers: [
-    {provide: DateAdapter, useClass: AppDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS},
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
   ]
 })
 
